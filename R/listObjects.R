80a5b77ddd0e7a069e4d9898eaec9f8915cfdc91
alexsanchezpla <asanchez@ub.edu>
2023-08-25 17:28:35 +0200

    Added listObjects function

---
+++ R/listObjects.R
@@ -0,0 +1,40 @@
+#' List objects in a binary R data file
+#'
+#' This function loads a binary R data file and returns a data frame
+#' with information about the contained objects.
+#'
+#' @param fileName Character string specifying the name of the binary R data file.
+#'
+#' @return A data frame with columns: "ObjectName", "ObjectType", "ObjectDimensions".
+#'
+#' @examples
+#' X<- matrix(runif(100), nrow=20)
+#' Y<- data.frame(Y1=rnorm(10), Y2=rpois(10, lambda=1), sex=sample(c(0,1), 10, replace=TRUE))
+#' Z=c(X, Y)
+#' save(X,Y,Z, file="binData.Rda")
+#' listObjects(fileName = "binData.Rda")
+#'
+#' @importFrom methods as
+#' @importFrom utils head
+#'
+#' @export
+listObjects <- function(fileName) {
+  loaded_objects <- get(load(fileName))
+  object_info <- data.frame(
+    ObjectName = character(),
+    ObjectType = character(),
+    ObjectDimensions = character(),
+    stringsAsFactors = FALSE
+  )
+  names4Cols <- colnames(object_info)
+  for (obj_name in names(loaded_objects)) {
+    obj <- loaded_objects[[obj_name]]
+    obj_type <- class(obj)[1]
+    obj_dims <- paste(dim(obj), collapse = "x")
+    object_info <- rbind(object_info, c(obj_name, obj_type, obj_dims))
+  }
+  colnames(object_info) <- names4Cols
+
+  return(object_info)
+}
+
